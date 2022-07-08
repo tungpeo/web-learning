1da1f4d41262f2224585e6035b394adafe5fe51c
tungpeo <tungpeo4@gmail.com>
2022-07-08 17:22:32 +0700

    buoi16

--- Buoi-16/js/Bai-1.js
+++ Buoi-16/js/Bai-1.js
@@ -14,17 +14,17 @@ function Bai1() {
     }
 }
 
-function Bai2(){
+function Bai2() {
     let a = document.getElementById("A").value;
     let n = document.getElementById("son");
     // 
     let sum = 0;
     let value = 1;
-    while(sum <= a){
+    while (sum <= a) {
         sum += value;
         value++;
     }
-    if(sum <= a){
+    if (sum <= a) {
         n = value;
     }
     console.log(sum);
@@ -32,17 +32,17 @@ function Bai2(){
     n.innerHTML = "N = " + n;
 }
 
-function Bai3(){
+function Bai3() {
     let n = document.getElementById("chuson").value;
     let chusotrongn = document.getElementById("chusotrongn");
     let tongchuson = document.getElementById("tongchuson");
     let daonguoc = document.getElementById("daonguoc");
-    let array = n.split('').map(i=>parseInt(i));
+    let array = n.split('').map(i => parseInt(i));
 
     console.log(array);
 
     let sum = 0;
-    for(let i = 0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         sum += array[i];
         console.log(sum);
     }
@@ -50,4 +50,46 @@ function Bai3(){
     chusotrongn.innerHTML = array.toString();
     tongchuson.innerHTML = sum;
     daonguoc.innerHTML = n.split('').reverse().join('');
+}
+
+function Bai4() {
+    let mang = document.getElementById("mang").value;
+    let soluongduong = document.getElementById("soluongduong");
+    let soluongam = document.getElementById("soluongam");
+    let duongmax = document.getElementById("duongmax");
+    let ammax = document.getElementById("ammax");
+    taomang = mang.split(' ');
+    let mangam = [];
+    let mangduong = [];
+    let count = 0;
+    let count2 = 0;
+    // 
+    for (let i = 0; i < taomang.length; i++) {
+        if (taomang[i] > 0) {
+            mangduong.push(taomang[i]);
+            count++;
+        }
+    }
+    for (let i = 0; i < taomang.length; i++) {
+        if (taomang[i] < 0) {
+            mangam.push(taomang[i]);
+            count2++;
+        }
+    }
+    console.log('mang duong: ' + mangduong);
+    console.log('mang am: ' + mangam);
+    soluongduong.innerHTML = 'So luong duong: ' + count;
+    soluongam.innerHTML = 'So luong am: ' + count2;
+
+    let max = mangduong[0];
+    let min = mangduong[0];
+    for (let i = 0; i < mangduong.length; i++) {
+        if (max < mangduong[i]) {
+            max = mangduong[i];
+        }
+    }
+    
+    duongmax.innerHTML = 'Dương Max: ' + max;
+    ammax.innerHTML = 'Dương Min: ' + min;
+
 }
