2636a3c28f0679e1b00896cfe4d73e7dc371ba15
tungpeo <tungpeo4@gmail.com>
2022-07-20 09:16:59 +0700

    Buoi-18

--- Buoi-18/js/index.js
+++ Buoi-18/js/index.js
@@ -1,39 +1,65 @@
-let dic = new Map();
+// let dic = new Map();
 
-dic.set("1", "HTML") //keys, value
-dic.set("2", "CSS")
-dic.set("3", "Javascript")
+// dic.set("1", "HTML") //keys, value
+// dic.set("2", "CSS")
+// dic.set("3", "Javascript")
 
-console.log(dic.keys())
-console.log(dic.values())
-console.log(dic.entries())
+// console.log(dic.keys())
+// console.log(dic.values())
+// console.log(dic.entries())
 
-// 
+// // 
 
-const valueSet = new Set(["Pham", "Quang", "Sang"])
-console.log(valueSet.values())
-for(let i in valueSet.values()){
-    console.log(valueSet.values(i));
-}
+// const valueSet = new Set(["Pham", "Quang", "Sang"])
+// console.log(valueSet.values())
+// for (let i in valueSet.values()) {
+//     console.log(valueSet.values(i));
+// }
 
 function SNT(n) {
-    let flag = true;
-    if (n < 2) flag = false;
+    if (n < 2) return false;
     else {
         for (let i = 0; i < n - 1; i++) {
-            if(n % i == 0){
-                flag = false;
-                break;
+            if (n % i == 0) {
+                return false;
             }
         }
     }
-    if(flag == true) console.log("HHm")
+}
+
+function HH(n) {
+    let s = 0;
+    for (let i = 0; i < n / 2; i++) {
+        if (n % i == 0) s += i;
+    }
+    if (s == n) return true;
+    else return false;
 }
 
 function Bai5() {
     let n = parseInt(document.getElementById("son").value);
-    let ai = parseInt(document.getElementById("son").value);
+    let ai = document.getElementById("ai").value;
+    let output = document.getElementById("output");
+
     mang = ai.split(' ').map(i => parseInt(i));
+    let check = [];
+    let checklength = mang.length;
 
+    if (checklength > n || checklength < n) alert("Gia tri khong khop voi n");
+    else if (checklength == n) {
+        for (let i = 0; i < mang.length; i++) {
+            if(SNT(mang[i])){
+                mang[i] = "NT";
+            }
+            else if (mang[i] % 2 == 0) {
+                mang[i] = "SC";
+            } else if (mang[i] % 2 !=0) {
+                mang[i] = "SL";
+            }
 
+        }
+
+    }
+    console.log(mang);
+    output.innerHTML = "Output: " + mang;
 }
