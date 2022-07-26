f8358edd2d53ba4d59d3a03b50e01962b8bceacd
tungpeo <tungpeo4@gmail.com>
2022-07-26 23:25:08 +0700

    weather-01

---
+++ Buoi-20/js/weather.js
@@ -0,0 +1,10 @@
+
+
+let lat, lon;
+
+let feellike = document.querySelector('.feel-like');
+let wind = document.querySelector('.feel-like');
+let visibility = document.querySelector('.feel-like');
+let barometer = document.querySelector('.feel-like');
+let humidity = document.querySelector('.feel-like');
+let dew-point = document.querySelector('.feel-like');
